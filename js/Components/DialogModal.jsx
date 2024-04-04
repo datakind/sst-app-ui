18d13529fbc63e46ee2d5fda921c1b7a1e51a159
Sophia Harms <sophia@datakind.org>
2024-04-04 10:36:20 -0600

    Reinstall stack and dependencies

---
+++ js/Components/DialogModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Modal from '@/Components/Modal';
+DialogModal.Content = function DialogModalContent({ title, children, }) {
+    return (<div className="px-6 py-4">
+      <div className="text-lg font-medium text-gray-900">
+        {title}
+      </div>
+
+      <div className="mt-4 text-sm text-gray-600">
+        {children}
+      </div>
+    </div>);
+};
+DialogModal.Footer = function DialogModalFooter({ children, }) {
+    return (<div className="px-6 py-4 bg-gray-100 text-right">
+      {children}
+    </div>);
+};
+export default function DialogModal({ children, ...modalProps }) {
+    return <Modal {...modalProps}>{children}</Modal>;
+}
---
+++ js/Components/InputError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function InputError({ message, className, children, }) {
+    if (!message && !children) {
+        return null;
+    }
+    return (<div className={className}>
+      <p className="text-sm text-red-600">
+        {message || children}
+      </p>
+    </div>);
+}
